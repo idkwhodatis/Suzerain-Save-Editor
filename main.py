196171147eedd77d9c14d99d21e703fc0a179e77
idkwhodatis <xvshuren@gmail.com>
2025-09-23 19:02:11 -0400

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+import dearpygui.dearpygui as dpg
+
+
+dpg.create_context()
+dpg.create_viewport(title='Suzerain Save Editor',width=1200,height=800)
+
+with dpg.window(tag="Main Window"):
+    dpg.add_text("Hello world")
+    dpg.add_button(label="Save")
+    dpg.add_input_text(label="string")
+    dpg.add_slider_float(label="float")
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.set_primary_window("Main Window",True)
+dpg.start_dearpygui()
+dpg.destroy_context()
